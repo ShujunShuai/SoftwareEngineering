0a012da3629e266868d0315b07d2ae9086dc53e2
Shujun Shuai <1059215293@qq.com>
2018-05-23 16:29:05 +0800

    second

--- SoftwareWeb/src/servlet/UpdateServlet.java
+++ SoftwareWeb/src/servlet/UpdateServlet.java
@@ -2,7 +2,8 @@ package servlet;
 
 import java.io.IOException;  
 import java.io.PrintWriter;  
-  
+
+import javax.servlet.http.HttpServlet; 
 import javax.servlet.ServletException;  
 import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;  
